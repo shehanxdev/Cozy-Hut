04b7db497f3c23a4ca116173eb7a0edcc4462228
Shehan Chanuka <shehanxdev@gmail.com>
2022-12-17 14:11:30 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import { Express } from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";

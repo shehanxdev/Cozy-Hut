9bc44f6e7a13ab8091fa2165af8db688e12995bf
Shehan Chanuka <shehanxdev@gmail.com>
2022-12-19 12:15:06 +0530

    Index.js is being coded

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -7,3 +7,22 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+app.listen(8080, () => {
+  console.log(__filename + "\n" + import.meta.url + "\n" + __dirname);
+});

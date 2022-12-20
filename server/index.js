3d30e435fafc75853be44f6ca5aaa90107cfebfc
Shehan Chanuka <shehanxdev@gmail.com>
2022-12-20 16:48:34 +0530

    File Storage setup

--- server/index.js
+++ server/index.js
@@ -23,6 +23,16 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+/*STORAGE CONFIG */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 app.listen(8080, () => {
   console.log(__filename + "\n" + import.meta.url + "\n" + __dirname);
 });

3026b5f462654dc60c96ddc61bc780490c7f715d
Shehan Chanuka <shehanxdev@gmail.com>
2023-01-03 07:39:18 +0530

    temp

---
+++ server/routes/user.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {verify} from "../middleware/auth.js";
+import{getUser,getFriends,addRemoveFriends} from "../controllers/user.js";
+const router=express.Router();
+
+//READ
+router.get("/:id",verify,getUser);
+router.get("/:id/friends",verify,getFriends);
+
+//UPDATE
+router.patch(":id/:friendsID",addRemoveFriends);
+
+export default router;

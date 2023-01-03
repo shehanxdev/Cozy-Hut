e96dfe5b08c45e154be04eae260b2a34594256f0
Shehan Chanuka <shehanxdev@gmail.com>
2023-01-04 00:05:47 +0530

    Added addRemoveFriends endpont without testing

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 
+//FETCH USER
 export const getUser = async (req, res) => {
   try {
     const { userId } = req.params;
@@ -10,6 +11,7 @@ export const getUser = async (req, res) => {
   }
 };
 
+//FETCH FRIENDS
 export const getFriends = async (req, res) => {
   try {
     const { id } = req.params;
@@ -22,13 +24,63 @@ export const getFriends = async (req, res) => {
       //formats to send only neccessary data
       const formattedfriends = friends.map(
         ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-          return{
-          _id, firstName, lastName, occupation, location, picturePath};
+          return {
+            _id,
+            firstName,
+            lastName,
+            occupation,
+            location,
+            picturePath,
+          };
         }
       );
       res.status(200).json({ friends: formattedfriends });
     } else {
       res.status(404).send("User does not exist");
+    }
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+//FRIEND OR UNFRIEND FRIENDS
+export const addRemoveFriends = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { friendsID } = req.params;
+
+    const user = await User.findById(id);
+    const friend = await User.findById(friendsID);
+    if (user && friend) {
+      if (user.friends.includes(friendsID)) {
+        //**friendid is the each input which will be given to the function inside the filter method
+        user.friends = user.friends.filter((friendid) => friendid != friendsID);
+        friend.friends = friend.friends.filter((friendid) => friendid != id);
+      } else {
+        user.friends.push(friendsID);
+        friend.friends.push(id);
+      }
+      await user.save();
+      await friend.save();
+
+      const friendList = await Promise.all(
+        user.friends.map((friendId) => User.findById(friendId))
+      );
+      const formattedfriends = friendList.map(
+        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+          return {
+            _id,
+            firstName,
+            lastName,
+            occupation,
+            location,
+            picturePath,
+          };
+        }
+      );
+      res.status(200).json(formattedfriends);
+    } else {
+      return res.status(404).send("User does not exist");
     }
   } catch (error) {
     res.status(404).json({ error: error.message });

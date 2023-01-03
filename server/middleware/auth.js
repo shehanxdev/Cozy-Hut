fdc6021a2cb0f5f641798a5e87711e8d6260aa16
Shehan Chanuka <shehanxdev@gmail.com>
2023-01-03 06:45:54 +0530

    Created verification middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+import { Jwt } from "jsonwebtoken";
+
+export const verify=(req,res,next)=>{
+    try {
+        let token =req.header("Authorization");
+        if(!token){
+            res.status(403).send("Access denied");
+        }
+        if(token.startsWith("Bearer")){
+            token=token.slice(7,token.length).trimLeft();
+        }
+
+        const verified=Jwt.verify(token,process.env.JWT_SECRET);
+        req.user=verified;
+        next();
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+}

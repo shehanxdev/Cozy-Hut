1729f0582dbb40fbc04662d229279bc5b4e6b092
Shehan Chanuka <shehanxdev@gmail.com>
2023-01-30 03:10:57 +0530

    Created layout files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage/HomePage";
+import LoginPage from "scenes/loginPage/LoginPage";
+import ProfilePage from "scenes/ProfilePage/ProfilePage";
+import NavBar from "scenes/NavBar/NavBar";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" component={LoginPage} />
+          <Route path="/home" component={HomePage} />
+          <Route path="/profile/:userId" component={ProfilePage} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
+//Write Routes for HomePage,LoginPage,ProfilePage using BrowserRouter,Routes and Route

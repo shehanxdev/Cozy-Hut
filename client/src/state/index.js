980dc730827bdda3e016f8e0b5fc5e136bedb088
Shehan Chanuka <shehanxdev@gmail.com>
2023-01-30 06:38:57 +0530

    Configured reducers

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,1 +1,53 @@
-//yet to be developed
+import { createSlice } from "@reduxjs/toolkit";
+
+const stateSlice = createSlice({
+  name: "state",
+  initialState: {
+    user: null,
+    mode: "light",
+    token: null,
+    posts: [],
+  },
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode == "light" ? "dark" : "light";
+    },
+    setLoggedIn: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLoggedOut: (state) => {
+      (state.user = null), (state.token = null);
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("User is not loggedin");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id == action.payload.post._id) {
+          return action.payload.post;
+        } else {
+          return post;
+        }
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const {
+  setMode,
+  setLoggedIn,
+  setLoggedOut,
+  setFriends,
+  setPosts,
+  setPost,
+} = stateSlice.actions;
+export default stateSlice.reducer;

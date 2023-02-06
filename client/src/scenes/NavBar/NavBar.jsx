5ba7b5412319374ab483c6ad868c1f98567d1563
Shehan Chanuka <shehanxdev@gmail.com>
2023-02-06 13:19:38 +0530

    On Navbar

--- client/src/scenes/NavBar/NavBar.jsx
+++ client/src/scenes/NavBar/NavBar.jsx
@@ -1,10 +1,13 @@
 import FlexBetween from "componenets/FlexBetween";
 import React from "react";
-
+import { useTheme } from "@mui/material";
 function NavBar() {
+  //COLOR CONSTANTS
+  const theme = useTheme();
+  const alt = theme.palette.background.alt;
   return (
-    <FlexBetween>
-      <FlexBetween></FlexBetween>
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem"></FlexBetween>
     </FlexBetween>
   );
 }
